e2f846e0f79e27c0e40d3d74322bf1faeb4b28c3
Touchsung <jettapat.th@gmail.com>
2022-10-28 15:20:33 +0700

    fix: navbar

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -2,8 +2,8 @@ import React from "react";
 
 export const NavBar = () => {
     return (
-        <div className="static bg-rose-600 h-10 w-screen">
-            <h2 className=" bg-rose-600 h-10 flex justify-center items-center rounded-3xl mx-3 text-white font-medium">Food-Order</h2>
+        <div className="static bg-rose-600 h-10">
+            <h2 className=" bg-rose-600 h-10 flex justify-center items-center rounded-3xl text-white font-medium">Food-Order</h2>
         </div>
     )
 };
